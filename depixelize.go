5b323d2237aa1bcd7b2520aba63de447b87901b0
Justin Awrey <awreyjustin@gmail.com>
2019-06-12 18:55:58 -0700

    implement dissimilar traverse

--- depixelize.go
+++ depixelize.go
@@ -1,6 +1,26 @@
 package depixelize
 
-import "image/color"
+import (
+	"image/color"
+	"math"
+)
+
+const (
+	yThresh = 0.18823529411
+	uThresh = 0.02745098039
+	vThresh = 0.02352941176
+)
+
+func dissimilar(n1, n2 *node) bool {
+	y1, u1, v1 := n1.pixel.yuv()
+	y2, u2, v2 := n2.pixel.yuv()
+	y3, u3, v3 := float64(y1), float64(u1), float64(v1)
+	y4, u4, v4 := float64(y2), float64(u2), float64(v2)
+
+	return math.Abs(y3-y4) > yThresh ||
+		math.Abs(u3-u4) > uThresh ||
+		math.Abs(v3-v4) > vThresh
+}
 
 // edge directions
 const (
@@ -46,8 +66,24 @@ func (g graph) traverse(onEach func(n *node, i, j int)) {
 	}
 }
 
+func (g graph) traverse2(onEach func(n2 *node2)) {
+	//TODO:
+}
+
 func (g graph) hasNodeAt(i, j int) bool {
 	return j < g.h && i < g.w
+}
+
+func (g graph) disconnectDissimilar() {
+	g.traverse(func(n *node, i, j int) {
+		for i := 0; i < 8; i++ {
+			if neighbour := n.getAdjacentNode(i); neighbour != nil {
+				if dissimilar(n, neighbour) {
+					n.setEdge(i, false)
+				}
+			}
+		}
+	})
 }
 
 type node struct {
@@ -58,14 +94,29 @@ type node struct {
 	j      int
 }
 
-func (n *node) setEdge(dir int, to bool) {
-	n.edges[dir] = to
+type node2 struct {
+	parent graph
+	tl     *node
+	tr     *node
+	bl     *node
+	br     *node
+}
 
+func (n *node) getAdjacentNode(dir int) *node {
 	connection := opposites[dir]
-	oppDir, i, j := connection.dir, connection.ix+n.i, connection.jx+n.j
+	i, j := connection.ix+n.i, connection.jx+n.j
 
 	if n.parent.hasNodeAt(i, j) {
-		n.parent.contents[j][i].edges[oppDir] = to
+		return n.parent.contents[j][i]
+	}
+	return nil
+}
+
+func (n *node) setEdge(dir int, to bool) {
+	if neighbour := n.getAdjacentNode(dir); neighbour != nil {
+		n.edges[dir] = to
+		oppDir := opposites[dir].dir
+		neighbour.edges[oppDir] = to
 	}
 }
 
@@ -93,5 +144,6 @@ type pixel struct {
 func (p *pixel) yuv() (y, u, v uint8) {
 	r, g, b, _ := p.color.RGBA()
 	r8, g8, b8 := uint8(r), uint8(g), uint8(b)
+
 	return color.RGBToYCbCr(r8, g8, b8)
 }
